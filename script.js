54b0c8f06e6c3af818dbfcc2b599a48a484af659
Izabelle Olofsson <izabelleolofsson@live.se>
2022-10-01 10:43:03 +0200

    Början på arbetet

---
+++ script.js
@@ -0,0 +1,32 @@
+form.addEventListener('submit', function(event){
+     event.preventDefault();
+    
+})
+
+const randomNumber = Math.ceil(Math.random()*100)
+console.log(randomNumber);
+
+function getValue(){
+
+    const numberGuessed = document.getElementById("numberGuessed").value;
+     if(randomNumber == numberGuessed){
+      
+        document.getElementById('test').innerHTML = 'Det var skrivet i stjärnorna att du skulle gissa rätt!'
+        console.log('Rätt');
+     }
+    else if(randomNumber < numberGuessed){
+    
+        document.getElementById('test').innerHTML = 'Den som siktar mot månen hamnar bland sjärnorna...gissa lägre!'
+        console.log('lägre');
+    }
+    else if(randomNumber > numberGuessed){
+        
+        document.getElementById('test').innerHTML = 'Venus står i retrograd, gissa högre!'
+        console.log('högre');
+    }
+    else{
+        text = 'Nu blev något knasigt, testa igen';
+    }
+    
+}
+
