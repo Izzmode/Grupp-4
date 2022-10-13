d24be2409219c63b318157b561eb653ce20014a7
Narek Pogosian <narekpogosian2@gmail.com>
2022-10-13 09:37:33 +0200

    la till antal försök och ändrade lite

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
+// Selectors
+const form = document.querySelector("#form");
+const test = document.querySelector("#test");
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  getValue();
 });
 
 const randomNumber = Math.ceil(Math.random() * 100);
-console.log(randomNumber);
+// console.log(randomNumber);
+let guessCount = 0;
 
 function getValue() {
-  const numberGuessed = document.getElementById("numberGuessed").value;
+  const numberGuessed = Number(document.getElementById("numberGuessed").value);
+  guessCount++;
   if (randomNumber == numberGuessed) {
-    document.getElementById("test").innerHTML =
-      "Det var skrivet i stjärnorna att du skulle gissa rätt!";
+    test.textContent = `Det var skrivet i stjärnorna att du skulle gissa rätt! Antal försök: ${guessCount}`;
     console.log("Rätt");
   } else if (randomNumber < numberGuessed) {
-    document.getElementById("test").innerHTML =
+    test.textContent =
       "Den som siktar mot månen hamnar bland sjärnorna...gissa lägre!";
     console.log("lägre");
   } else if (randomNumber > numberGuessed) {
-    document.getElementById("test").innerHTML =
-      "Venus står i retrograd, gissa högre!";
+    test.textContent = "Venus står i retrograd, gissa högre!";
     console.log("högre");
   } else {
-    text = "Nu blev något knasigt, testa igen";
+    test = "Nu blev något knasigt, testa igen";
   }
 }
