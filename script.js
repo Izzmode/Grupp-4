b49722ec82d46ff62a5de115ae1d17cdbdb327e4
Narek Pogosian <narekpogosian2@gmail.com>
2022-10-13 09:13:34 +0200

    uppdatera

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-form.addEventListener('submit', function(event){
-     event.preventDefault();
-    
-})
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  getValue();
+});
 
-const randomNumber = Math.ceil(Math.random()*100)
+const randomNumber = Math.ceil(Math.random() * 100);
 console.log(randomNumber);
 
-function getValue(){
-
-    const numberGuessed = document.getElementById("numberGuessed").value;
-     if(randomNumber == numberGuessed){
-      
-        document.getElementById('test').innerHTML = 'Det var skrivet i stjärnorna att du skulle gissa rätt!'
-        console.log('Rätt');
-     }
-    else if(randomNumber < numberGuessed){
-    
-        document.getElementById('test').innerHTML = 'Den som siktar mot månen hamnar bland sjärnorna...gissa lägre!'
-        console.log('lägre');
-    }
-    else if(randomNumber > numberGuessed){
-        
-        document.getElementById('test').innerHTML = 'Venus står i retrograd, gissa högre!'
-        console.log('högre');
-    }
-    else{
-        text = 'Nu blev något knasigt, testa igen';
-    }
-    
+function getValue() {
+  const numberGuessed = document.getElementById("numberGuessed").value;
+  if (randomNumber == numberGuessed) {
+    document.getElementById("test").innerHTML =
+      "Det var skrivet i stjärnorna att du skulle gissa rätt!";
+    console.log("Rätt");
+  } else if (randomNumber < numberGuessed) {
+    document.getElementById("test").innerHTML =
+      "Den som siktar mot månen hamnar bland sjärnorna...gissa lägre!";
+    console.log("lägre");
+  } else if (randomNumber > numberGuessed) {
+    document.getElementById("test").innerHTML =
+      "Venus står i retrograd, gissa högre!";
+    console.log("högre");
+  } else {
+    text = "Nu blev något knasigt, testa igen";
+  }
 }
-
